33274a7d0b292dd57c286c561e303efc9ef15f5a
GianlorenzoDercole <johndercole01@gmail.com>
2023-01-26 19:24:31 -0800

    added image field see models.py and serializers.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 from rest_framework import routers
 from movies.views import MovieViewSet,ActionViewSet,ComedyViewSet
-
+from django.conf.urls.static import static
+from django.conf import settings
 # router = routers.DefaultRouter()
 router = routers.SimpleRouter()
 router.register('movies',MovieViewSet)
@@ -28,4 +29,4 @@ router.register('comedy',ComedyViewSet)
 urlpatterns = [
     path('',include(router.urls)),
     path('admin/', admin.site.urls),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
